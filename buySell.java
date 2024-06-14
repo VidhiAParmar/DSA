e046bad9e6dfe2d1d92382d1ab20f8b7de6ac1df
Vidhi Parmar <115219953+VidhiAParmar@users.noreply.github.com>
2024-06-14 15:58:23 +0530

    Update buySell.java

--- buySell.java
+++ buySell.java
@@ -1,4 +1,16 @@
-
+/*
+You are given an array of prices where prices[i] is the price of a
+given stock on an ith day.
+You want to maximize your profit by choosing a single day to buy
+one stock and choosing a different day in the future to sell that
+stock.
+Return the maximum profit you can achieve from this transaction.
+If you cannot achieve any profit, return 0
+Input: prices = [9,2,5,3,1,4]
+Output: 3
+Explanation: Buy on day 5 (price = 1) and sell on day 6 (price = 4),
+profit = 4-1 = 3.
+*/
 public class buySell {
    public static void main(String args[]){
         int[] arr = {9,2,5,3,1,4};
